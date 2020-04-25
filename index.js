dc57d388f4959620f87f06cf38738e5030c10424
Christopher Nethercott <ccnethercott@gmail.com>
2020-04-25 18:41:02 +0100

    Create js project

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const axios = require('axios').default;
+
+const app = express();
+const config = require('./config.json');
+const package = require('./package.json');
+
+let status = [];
+
+app.get('/', (req, res) => {
+    res.json({
+       "currently_stored": status.length,
+        "currently_on": (status.filter(v => v).length),
+        "currently_off": (status.filter(v => !v).length),
+        "statuses": status
+    });
+});
+
+app.get('/:id/ison', (req, res) => {
+    let id = req.params.id;
+    if (config.debug) console.log(`Got ison with id ${id}, from ${req.ip}`);
+
+    status[id] = true;
+
+    res.send("Well Done!");
+})
+
+
+app.listen(config.port || 80);
