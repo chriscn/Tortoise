357764492de5ca3461562db30786205004de2a9f
Christopher Nethercott <ccnethercott@gmail.com>
2020-04-27 14:05:39 +0100

    Remove unused parameter

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use('/', id);
 
-app.use((req, res, next) => {
+app.use((req, res) => {
     res.status(404).json({
         "status": "not-found",
         "error": 404
