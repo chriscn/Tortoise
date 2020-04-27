9d84fef4377e4a2d1a58aad292fb014527dd9583
Christopher Nethercott <ccnethercott@gmail.com>
2020-04-27 11:46:56 +0100

    Should expect some json response

--- test/index.test.js
+++ test/index.test.js
@@ -2,9 +2,10 @@ const request = require('supertest');
 const app = require('../index.js');
 
 describe('GET /', function () {
-    it('return 200 response', function () {
+    it('return JSON and 200 response', function () {
         return request(app)
             .get('/')
+            .expect('Content-Type', /json/)
             .expect(200)
     })
 })
